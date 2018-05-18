0ebdd21046a819a52b71c4f1c3706cb763226571
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-18 13:42:17 +0100

    Studio class set t up

---
+++ src/main/java/models/Studio.java
@@ -0,0 +1,61 @@
+package models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+
+@Entity
+@Table(name"studios")
+public class Studio {
+
+    private int id;
+    private String name;
+    private Set<Film> films;
+    private double budget;
+
+    public Studio() {
+    }
+
+    public Studio(String name, double budget) {
+        this.name = name;
+        this.budget = budget;
+        this.films = new HashSet<Film>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "studio")
+    public Set<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(Set<Film> films) {
+        this.films = films;
+    }
+
+    @Column(name = "budget")
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+}

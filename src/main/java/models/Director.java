c4fe7e72b24dc2afe2f74f4fd42f40562df121df
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-18 14:30:31 +0100

    Director class set up

---
+++ src/main/java/models/Director.java
@@ -0,0 +1,53 @@
+package models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "directors")
+public class Director {
+
+    private int id;
+    private String name;
+    private Set<Film> films;
+
+    public Director() {
+    }
+
+    public Director(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.films = new HashSet<Film>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "director")
+    public Set<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(Set<Film> films) {
+        this.films = films;
+    }
+
+}
